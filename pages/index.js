4b7ddf10aedecd1af27818dbd17cb47a0a1824ad
kushmishraa <mishrash64@gmial.com>
2023-03-18 03:14:11 +0530

    added google adsense

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import Header from "@/component/Header";
 import { reducer } from "@/reducer/appReducer";
 import HeroSection from "@/component/HeroSection";
 import ListingPage from "@/component/ListingPage";
+import Script from "next/script";
 import Head from "next/head";
+import { useEffect } from "react";
 export default function Index(){
 
   const initialState = {}
@@ -15,15 +17,41 @@ export default function Index(){
   const scrollToListing = () =>{
     listingRef.current.scrollIntoView({behavior : 'smooth'});
   }
+const adsenseRef = useRef();
 
+  useEffect(() => {
+    window.adsbygoogle = window.adsbygoogle || []
+    window.adsbygoogle.push({})
+  })
+
+  const handleAdClose = () =>{
+    adsenseRef.current.style.display = 'none';
+  }
   return(
     
     <div className="main-container">
-      <Head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3907583389552514"
-     crossOrigin="anonymous"></script></Head>
+
         <div className="main-header">
         <Header scrollToListing={scrollToListing} />
         </div>
+      <div className="googleAdsense-script" ref={adsenseRef}>
+      <h2 className="close-googleadsense" onClick={handleAdClose}>x</h2>
+      <Head>
+      <title>jobFinder</title>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3907583389552514"
+      crossOrigin="anonymous"></script>
+      </Head>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3907583389552514"
+     crossorigin="anonymous"></Script>
+     <div>
+     <ins className="adsbygoogle"
+     style={{display:"block"}}
+     data-ad-client="ca-pub-3907583389552514"
+     data-ad-slot="2216689906"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+     </div>
+      </div>
         <div className="main-heroSection">
         <HeroSection scrollToListing={scrollToListing}/>
         </div>
