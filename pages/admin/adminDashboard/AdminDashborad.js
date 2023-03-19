786b35742bf38be4ee8e2aec26b7580020313109
kushmishraa <mishrash64@gmial.com>
2023-03-19 17:05:29 +0530

    build errors fixed

--- pages/admin/adminDashboard/AdminDashborad.js
+++ pages/admin/adminDashboard/AdminDashborad.js
@@ -12,25 +12,32 @@ export default function AdminDashboard(props){
     
     useEffect(()=>{
         getData(null,listedjobobjContext.dispatcher , listedjobobjContext.listedjobobj , "setData");
-    })
+    },[isLoggin])
+    
+    const fincNoOfListing = (listedjobobjContext) => {
+        if(listedjobobjContext && listedjobobjContext.listedjobobj.listedJobs){
+            return listedjobobjContext.listedjobobj.listedJobs.length
+        }
+    }
 
     return(
         <div className="admin-dashboard-container">
           <div className="admin-dashboard-currlisting">
-                {Object.keys(listedjobobjContext.listedjobobj).length > 0 ? listedjobobjContext.listedjobobj.listedJobs.map((jobObj)=>{
+                { fincNoOfListing(listedjobobjContext) > 0 ? 
+                listedjobobjContext.listedjobobj.listedJobs.map((jobObj)=>{
                     return(
                     <ListingComponent jobObj = {jobObj} isLoggin={isLoggin} key={jobObj.id}/>
                     )
-                }) : 
-                <Audio
-                    height="80"
-                    width="80"
-                    radius="9"
-                    color="blue"
-                    ariaLabel="loading"
-                    wrapperStyle
-                    wrapperClass
-                    />}
+                }) 
+                : <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="blue"
+                ariaLabel="loading"
+                wrapperStyle
+                wrapperClass
+                /> }
             </div>
         </div>
     )
