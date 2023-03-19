450ef2203c29a8ca7814ad1014d9d743227c477e
kushmishraa <mishrash64@gmial.com>
2023-03-20 02:43:31 +0530

    nothing improves

--- api/s3bucketGetPut.js
+++ api/s3bucketGetPut.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
+const url = 'https://vo4kyruihh.execute-api.eu-north-1.amazonaws.com/dev/directjobfinder/listing.json'
+const postUrl ='https://vo4kyruihh.execute-api.eu-north-1.amazonaws.com/dev/directjobfinder/listing.json'
 
 export async function putData(listedJobs){
-//          const settings = {
-//          method: 'PUT',
-//          headers: {
-//                 'Content-Type': 'application/json',
-//          },
-//          body : {listedJobs}
-//      };
-//    await fetch('https://vo4kyruihh.execute-api.eu-north-1.amazonaws.com/dev/directjobfinder/listing.json' , settings)
-     await axios.put('https://vo4kyruihh.execute-api.eu-north-1.amazonaws.com/dev/directjobfinder/listing.json' , {listedJobs})
+    const response = await fetch(postUrl , {method:'POST',headers:{'Content-Type' : 'application/json' , 'Action' : 'putdata'},body:{listedJobs}})
+    const data = await response.json();
+    console.log(data);
 }
 
 export async function getData(setListedJobs , dispatcher , initialState , action){
-    const response = await fetch('https://vo4kyruihh.execute-api.eu-north-1.amazonaws.com/dev/directjobfinder/listing.json')
+    const response = await fetch(url)
     const data = await response.json();
     
     if(!setListedJobs){
